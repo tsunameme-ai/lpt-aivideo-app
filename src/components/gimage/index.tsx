e4cb348a24f09ede41967378f2e11eee1a8350c3
CJ Jiang <cxjiang@gmail.com>
2024-02-07 09:06:37 -0500

    update image download api

--- src/components/gimage/index.tsx
+++ src/components/gimage/index.tsx
@@ -1,27 +1,56 @@
-import { Image } from "@nextui-org/react"
-import { useState } from "react"
+import { Button, Spinner, Image } from "@nextui-org/react"
+import { useEffect, useState } from "react"
 import ErrorComponent from "../error"
-
 
 interface GImageProps {
     src: string
     alt?: string
 }
-const GImage: React.FC<GImageProps> = (props: GImageProps) => {
-    const [errorMessage, setErrorMessage] = useState<string>('')
-    const onImageLoad = () => {
-        setErrorMessage('')
-    }
-    const onImageLoadError = () => {
-        setErrorMessage('Failed to fetch image')
-    }
 
+const GImage: React.FC<GImageProps> = (props: GImageProps) => {
+    const [isFetching, setIsFetching] = useState<boolean>(true)
+    const [errorMessage, setErrorMessage] = useState<string>('')
+    const [imageFileUrl, setImageFileUrl] = useState<string | undefined>(undefined)
+
+    useEffect(() => {
+        downloadImage()
+            .catch(console.error)
+    }, [])
+
+    const downloadImage = async () => {
+        setIsFetching(true)
+        try {
+            const res = await fetch('/api/image', {
+                method: 'POST',
+                body: JSON.stringify({ url: props.src }),
+            });
+
+            if (!res.ok) {
+                throw new Error(`Image download failed with status ${res.status}`);
+            }
+
+            const blob = await res.blob();
+            const fileUrl = URL.createObjectURL(blob);
+            setImageFileUrl(fileUrl);
+            setErrorMessage('');
+
+        } catch (error: any) {
+            console.error(error);
+            setErrorMessage(error.message);
+        }
+        finally {
+            setIsFetching(false)
+        }
+    }
     return (
         <>
-            <Image src={props.src} alt={props.alt} onLoad={onImageLoad} onError={onImageLoadError} />
-            {errorMessage.length > 0 && <ErrorComponent errorMessage={errorMessage} />}
+            {imageFileUrl && <Image alt={props.alt} src={imageFileUrl} />}
+            {isFetching && <Spinner />}
+            {errorMessage.length > 0 && <>
+                <ErrorComponent errorMessage={errorMessage} />
+                <Button onClick={downloadImage}>Retry</Button>
+            </>}
         </>
     )
 }
-
 export default GImage
