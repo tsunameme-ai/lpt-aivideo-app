c0b9a3a6e633086c18aebcc4beaf72df7f2d7463
CJ Jiang <cxjiang@gmail.com>
2024-02-13 17:20:26 -0500

    Add missing files

---
+++ src/components/advanced-indicator/index.tsx
@@ -0,0 +1,14 @@
+import { useGenerationContext } from "@/context/generation-context";
+import { Switch } from "@nextui-org/react"
+
+
+const AdvancedIndicator: React.FC = () => {
+    const { isAdvancedView, setIsAdvancedView } = useGenerationContext()
+    return (
+        <>
+            <Switch defaultSelected={isAdvancedView} aria-label="Advanced View" onValueChange={setIsAdvancedView}>Advanced Settings</Switch>
+        </>
+    );
+};
+
+export default AdvancedIndicator
