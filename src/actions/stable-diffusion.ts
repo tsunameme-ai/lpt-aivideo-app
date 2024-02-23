da4634650c2be3f9dfc784cdbc49d6b891b5a63b
CJ Jiang <cxjiang@gmail.com>
2024-02-23 15:46:42 -0500

    Add sd api

--- src/actions/stable-diffusion.ts
+++ src/actions/stable-diffusion.ts
@@ -1,35 +1,18 @@
 'use server'
 
-import { LivepeerAPI } from "@/libs/external/livepeer";
-import { LivepeerStaticAPI } from "@/libs/external/livepeer-static";
-import { GenerationOutput, Img2vidInput, Txt2imgInput } from "@/libs/types";
+import { SDAPI, SDStaticAPI } from "@/libs/sd-api";
+import { GenerationOutput, Img2vidNativeInput, Txt2imgInput } from "@/libs/types";
 
 export async function txt2img(params: Txt2imgInput): Promise<Array<GenerationOutput>> {
     if (process.env.NEXT_PUBLIC_LIVEPEER === 'static') {
-        return await new LivepeerStaticAPI().txt2img()
+        return await new SDStaticAPI().txt2img()
     }
-    return await new LivepeerAPI().txt2img(params)
+    return await new SDAPI().txt2img(params)
 }
 
-
-const fetchImageAsFile = async (url: string): Promise<File> => {
-    const response = await fetch(url, { cache: 'no-cache' })
-    if (response.ok) {
-        const blob = await response.blob()
-        const file = new File([blob], 'image.png', { type: 'image/png' })
-        return file
-    }
-    console.log(`--fetchImageAsFile response ${url} -- `)
-    console.log(response)
-    throw new Error(`Fetch image failed ${response.status}`)
-}
-
-export async function img2vid(params: Img2vidInput): Promise<Array<GenerationOutput>> {
+export async function img2vid(params: Img2vidNativeInput): Promise<Array<GenerationOutput>> {
     if (process.env.NEXT_PUBLIC_LIVEPEER === 'static') {
-        return await new LivepeerStaticAPI().img2vid()
+        return await new SDStaticAPI().img2vid()
     }
-    //Download image
-    const imgFile = await fetchImageAsFile(params.imageUrl!)
-    params.imageFile = imgFile
-    return await new LivepeerAPI().img2vid(params)
+    return await new SDAPI().img2vid(params)
 }
