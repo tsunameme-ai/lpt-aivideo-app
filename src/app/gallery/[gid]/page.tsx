e5f88143e9ad5d9abee3bcbc069537ce45295348
Kuan Huang <huangkuan@gmail.com>
2024-03-12 15:06:26 -0400

    push to dev for og test

--- src/app/gallery/[gid]/page.tsx
+++ src/app/gallery/[gid]/page.tsx
@@ -3,6 +3,7 @@ import GalleryItemComponent from '@/components/gallery-item'
 import { fetchGenerationData } from '@/actions/stable-diffusion'
 import { GenerationType, Txt2imgInput } from '@/libs/types'
 
+
 type Props = {
     params: { gid: string }
     searchParams: { [key: string]: string | string[] | undefined }
@@ -12,10 +13,11 @@ export async function generateMetadata(
     { params }: Props,
 ): Promise<Metadata> {
     try {
+        //fields ref https://stackoverflow.com/questions/76265976/next-js-dynamic-metadata
         const data = await fetchGenerationData(params.gid)
         if (data.type === GenerationType.TXT2IMG) {
             return {
-                //fields ref https://stackoverflow.com/questions/76265976/next-js-dynamic-metadata
+
                 description: (data.input as Txt2imgInput).pPrompt,
                 openGraph: {
                     images: (data.outputs || []).map(item => {
@@ -24,7 +26,10 @@ export async function generateMetadata(
                 },
             }
         }
+
         return {
+            title: 'Groove',
+            description: '',
             openGraph: {
                 videos: (data.outputs || []).map(item => {
                     return item.url
@@ -38,10 +43,12 @@ export async function generateMetadata(
     return {}
 }
 
+
+
 export default function Page({ params }: { params: { gid: string } }) {
+
     return (
         <>
-            <div>Under construction</div>
             <GalleryItemComponent generationId={params.gid} />
         </>
     )
