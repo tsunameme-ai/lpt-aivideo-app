c12a5da77cbc1d76cbd23f3f2d0f23befc667048
CJ Jiang <cxjiang@gmail.com>
2024-03-31 15:12:24 -0400

    Add load ready to video element

--- src/components/media-player/index.tsx
+++ src/components/media-player/index.tsx
@@ -15,7 +15,7 @@ const MediaPlayerComponent: React.FC<MediaPlayerComponentProps> = (props: MediaP
     }
     const [isVideo] = useState<boolean>(checkIsVideo(props.src))
     return (
-        isVideo ? <video className={props.className} autoPlay loop muted controls webkit-playsinline playsInline src={props.src} />
+        isVideo ? <video className={props.className} autoPlay loop muted controls webkit-playsinline playsInline src={props.src} onCanPlay={props.onLoad} />
             : <Image className={props.className} src={props.src} alt='image' onLoad={props.onLoad} />
     )
 }
