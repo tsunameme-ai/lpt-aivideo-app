aa1099baa4c15e727e8cdff8c975e0128a28b689
CJ Jiang <cxjiang@gmail.com>
2024-04-04 14:57:50 -0400

    Add installation strategy

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export const viewport: Viewport = {
   width: 'device-width',
   initialScale: 1,
   maximumScale: 1,
-  userScalable: false
+  // userScalable: false
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,27 @@
 // app/providers.tsx
 'use client'
 
+import { DisplayMode, Installer } from '@/components/installer';
 import { NextUIProvider } from '@nextui-org/react'
+import React from 'react';
+import { useState } from 'react';
+
 
 export function Providers({ children }: { children: React.ReactNode }) {
+    const [displayMode, setDisplayMode] = useState<DisplayMode>()
+    const onDisplayModeChange = (mode: DisplayMode) => {
+        console.log(`???onDisplayModeChange:${mode} `)
+        setDisplayMode(mode)
+    }
+
+
     return (
         <NextUIProvider>
-            {children}
+            <Installer onDisplayModeChange={onDisplayModeChange} />
+            {
+                displayMode !== DisplayMode.BROWSER &&
+                <React.Fragment>{children}</React.Fragment>
+            }
         </NextUIProvider>
-    )
+    );
 }
