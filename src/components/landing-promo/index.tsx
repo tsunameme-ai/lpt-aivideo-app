e515cb684d7e35ffe50e02b3394732947b695527
Kuan Huang <huangkuan@gmail.com>
2024-04-05 15:07:55 -0400

    rounded corner

--- src/components/landing-promo/index.tsx
+++ src/components/landing-promo/index.tsx
@@ -25,22 +25,22 @@ const LandingPromoComponent = () => {
             <div className='text-[20px]'>with <span className={styles.red}>generative AI</span></div>
             <Spacer y={8} />
             <Slider className={styles.slider} {...sliderGIFSettings}>
-                <div key={0}><Image src='https://lpt-aivideo-dst.s3.amazonaws.com/EtXjqe36bZ.gif' alt='' /> </div>
-                <div key={1}><Image src='https://lpt-aivideo-dst.s3.amazonaws.com/E5J8VQLt1X.gif' alt='' /></div>
-                <div key={2}><Image src='https://lpt-aivideo-dst.s3.amazonaws.com/jJ5VujUbMn.gif' alt='' /></div>
-                <div key={3}><Image src='https://lpt-aivideo-dst.s3.amazonaws.com/GH1Exd8DcI.gif' alt='' /></div>
-                <div key={4}><Image src='https://lpt-aivideo-dst.s3.amazonaws.com/ofw4eSWITF.gif' alt='' /></div>
-                <div key={5}><Image src='https://lpt-aivideo-dst.s3.amazonaws.com/b410c61Xiw.gif' alt='' /></div>
-                <div key={6}><Image src='https://lpt-aivideo-dst.s3.amazonaws.com/qo198RafvM.gif' alt='' /></div>
-                <div key={7}><Image src='https://lpt-aivideo-dst.s3.amazonaws.com/uLacJWG3y9.gif' alt='' /></div>
-                <div key={8}><Image src='https://lpt-aivideo-dst.s3.amazonaws.com/tUH3FrFYD9.gif' alt='' /></div>
-                <div key={9}><Image src='https://lpt-aivideo-dst.s3.amazonaws.com/Ir4l2Q7Sva.gif' alt='' /></div>
-                <div key={10}><Image src='https://lpt-aivideo-dst.s3.amazonaws.com/A7kXSaj0ec.gif' alt='' /></div>
-                <div key={11}><Image src='https://lpt-aivideo-dst.s3.amazonaws.com/d3SUEx6JZS.gif' alt='' /></div>
-                <div key={12}><Image src='https://lpt-aivideo-dst.s3.amazonaws.com/qElP3G9NwO.gif' alt='' /></div>
-                <div key={13}><Image src='https://lpt-aivideo-dst.s3.amazonaws.com/amwYmgjyTU.gif' alt='' /></div>
-                <div key={14}><Image src='https://lpt-aivideo-dst.s3.amazonaws.com/KGwZxKAw4u.gif' alt='' /></div>
-                <div key={15}><Image src='https://lpt-aivideo-dst.s3.amazonaws.com/riJglKynzV.gif' alt='' /></div>
+                <div key={0}><Image radius='sm' src='https://lpt-aivideo-dst.s3.amazonaws.com/EtXjqe36bZ.gif' alt='' /> </div>
+                <div key={1}><Image radius='sm' src='https://lpt-aivideo-dst.s3.amazonaws.com/E5J8VQLt1X.gif' alt='' /></div>
+                <div key={2}><Image radius='sm' src='https://lpt-aivideo-dst.s3.amazonaws.com/jJ5VujUbMn.gif' alt='' /></div>
+                <div key={3}><Image radius='sm' src='https://lpt-aivideo-dst.s3.amazonaws.com/GH1Exd8DcI.gif' alt='' /></div>
+                <div key={4}><Image radius='sm' src='https://lpt-aivideo-dst.s3.amazonaws.com/ofw4eSWITF.gif' alt='' /></div>
+                <div key={5}><Image radius='sm' src='https://lpt-aivideo-dst.s3.amazonaws.com/b410c61Xiw.gif' alt='' /></div>
+                <div key={6}><Image radius='sm' src='https://lpt-aivideo-dst.s3.amazonaws.com/qo198RafvM.gif' alt='' /></div>
+                <div key={7}><Image radius='sm' src='https://lpt-aivideo-dst.s3.amazonaws.com/uLacJWG3y9.gif' alt='' /></div>
+                <div key={8}><Image radius='sm' src='https://lpt-aivideo-dst.s3.amazonaws.com/tUH3FrFYD9.gif' alt='' /></div>
+                <div key={9}><Image radius='sm' src='https://lpt-aivideo-dst.s3.amazonaws.com/Ir4l2Q7Sva.gif' alt='' /></div>
+                <div key={10}><Image radius='sm' src='https://lpt-aivideo-dst.s3.amazonaws.com/A7kXSaj0ec.gif' alt='' /></div>
+                <div key={11}><Image radius='sm' src='https://lpt-aivideo-dst.s3.amazonaws.com/d3SUEx6JZS.gif' alt='' /></div>
+                <div key={12}><Image radius='sm' src='https://lpt-aivideo-dst.s3.amazonaws.com/qElP3G9NwO.gif' alt='' /></div>
+                <div key={13}><Image radius='sm' src='https://lpt-aivideo-dst.s3.amazonaws.com/amwYmgjyTU.gif' alt='' /></div>
+                <div key={14}><Image radius='sm' src='https://lpt-aivideo-dst.s3.amazonaws.com/KGwZxKAw4u.gif' alt='' /></div>
+                <div key={15}><Image radius='sm' src='https://lpt-aivideo-dst.s3.amazonaws.com/riJglKynzV.gif' alt='' /></div>
             </Slider>
             <div className='text-[12px]'>Selctions from the community</div>
         </>
