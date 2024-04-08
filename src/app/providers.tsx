d562d33da97bd526cb67ff6864da457c37fc03d5
Kuan Huang <huangkuan@gmail.com>
2024-04-08 17:04:04 -0400

    added a flag to disable pwa when debugging locally

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,21 +1,26 @@
 'use client'
 
-import { Installer } from '@/components/installer';
+import { Installer } from '@/components/installer'
 import { NextUIProvider } from '@nextui-org/react'
-import React from 'react';
-import { useState } from 'react';
+import React from 'react'
+import { useState } from 'react'
 
 
 export function Providers({ children }: { children: React.ReactNode }) {
     const [isAppReady, setIsAppReady] = useState<boolean>(false)
-
     return (
         <NextUIProvider>
-            <Installer onAppReadyChange={setIsAppReady} />
-            {
-                isAppReady &&
+            {process.env.NEXT_PUBLIC_DEBUG === 'browser' ?
                 <React.Fragment>{children}</React.Fragment>
+                :
+                <>
+                    <Installer onAppReadyChange={setIsAppReady} />
+                    {
+                        isAppReady &&
+                        <React.Fragment>{children}</React.Fragment>
+                    }
+                </>
             }
         </NextUIProvider>
-    );
+    )
 }
