e0c3d7f1ad6bfcad135180e19086644763e87777
CJ Jiang <cxjiang@gmail.com>
2024-04-17 12:15:28 -0400

    remove useless code

--- src/app/gallery/cellModal.tsx
+++ src/app/gallery/cellModal.tsx
@@ -14,7 +14,7 @@ const CellModal: React.FC<CellModalProps> = (props: CellModalProps) => {
 
     return (
         <>
-            <Modal size='sm' hideCloseButton={true} isOpen={props.isOpen} onClose={() => { props.onClose?.() }} placement="center" backdrop="blur" >
+            <Modal hideCloseButton={true} isOpen={props.isOpen} onClose={() => { props.onClose?.() }} placement="center" backdrop="blur" >
                 <ModalContent className="flex items-center">
                     <MediaPlayerComponent src={props.imgUrl} key={props.imgUrl} className={"w-full"} />
                     <FaShare className={styles.galleryShareIcon} onClick={() => props.handleShare?.(props.imgUrl)} />
