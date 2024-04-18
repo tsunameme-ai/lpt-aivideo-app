75e53812f63463b2a82187c93d79762a9e45711c
Kuan Huang <huangkuan@gmail.com>
2024-04-18 12:53:50 -0400

    try 512 post gif truncation fix

--- src/libs/sd-api.ts
+++ src/libs/sd-api.ts
@@ -43,7 +43,7 @@ export class SDAPI {
             "overlay_base64": params.overlayBase64,
             "overlay_text": params.overlayText,
             "image_generation_id": params.imageGenerationId,
-            "output_width": 384,
+            "output_width": 512,
             "user_id": params.userId
         }
         const url = process.env.NEXT_PUBLIC_API_ENDPOINT_IMG2VID!
