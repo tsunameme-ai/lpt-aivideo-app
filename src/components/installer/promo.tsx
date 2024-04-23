465785e4ddd30c0a108717d53aa845c2609ca55e
Kuan Huang <huangkuan@gmail.com>
2024-04-22 22:02:42 -0400

    testing the landing page

--- src/components/installer/promo.tsx
+++ src/components/installer/promo.tsx
@@ -1,13 +1,17 @@
-import { Spacer } from "@nextui-org/react"
-import LandingPromoComponent from '@/components/landing-promo'
+import { Spacer, Image } from "@nextui-org/react"
 import styles from '@/styles/home.module.css'
-import { FaAnglesDown } from "react-icons/fa6"
 import { appFont } from "@/app/fonts"
 import { SecondaryButton } from "../buttons"
+import { LuShare, LuAlignJustify } from "react-icons/lu"
+import { FaAnglesDown, FaAnglesUp } from "react-icons/fa6"
 
 interface InstallPromoProps {
     hasInstallPrompt: boolean
     isMobile: boolean
+    isChrome: boolean
+    isSafari: boolean
+    isBrave: boolean
+    isFireFox: boolean
     onInstallRequested: () => void
 }
 
@@ -17,8 +21,29 @@ export const InstallPromo: React.FC<InstallPromoProps> = (props: InstallPromoPro
         dynamicMSG = <p>Please open this page from your mobile phone.</p>
     else if (props.hasInstallPrompt) {
         dynamicMSG = <SecondaryButton onPress={props.onInstallRequested}>Install</SecondaryButton >
-    } else {
-        dynamicMSG = <><div>Tap below button and choose</div> <div><strong>Add to Home Screen</strong> to install the app</div></>
+    } else if (props.isFireFox) {
+        dynamicMSG = <>
+            <div className='flex items-center' >
+                <span>Please tap the</span>
+                &nbsp;
+                <LuAlignJustify />
+                &nbsp;
+                <span>button on your browser</span>
+            </div>
+            <div> and choose <strong>Add to Home Screen</strong> to install the app on your phone</div>
+        </>
+    }
+    else {
+        dynamicMSG = <>
+            <div className='flex items-center' >
+                <span>Please tap the</span>
+                &nbsp;
+                <LuShare />
+                &nbsp;
+                <span>button on your browser</span>
+            </div>
+            <div> and choose <strong>Add to Home Screen</strong> to install the app on your phone</div>
+        </>
     }
 
     return (
@@ -26,14 +51,38 @@ export const InstallPromo: React.FC<InstallPromoProps> = (props: InstallPromoPro
         <div className={`${styles.main} ${appFont.className} w-full min-h-svh bg-gradient-to-b from-50% from-primary to-[#98CDB1]`}>
             <div className={`h-20 w-full flex bg-img bg-repeat-x ${styles.wave}`} />
             <div className={`${styles.centerLanding} text-white`}>
-                <LandingPromoComponent />
+                <div className='font-bold text-background text-5xl'>TSUNAMEME</div>
+                <Spacer y={4} />
+                <div className='font-semibold text-background text-base'>
+                    <p>Making GIF expressions</p>
+                    <p>with generative AI</p>
+                </div>
+                <Spacer y={4} />
+                <div className='flex justify-center items-center'>
+                    <Image className="w-64" src='https://lpt-aivideo-dst.s3.amazonaws.com/bSO9pFGHzV.gif' alt='dog' />
+                </div>
                 <Spacer y={4} />
                 {dynamicMSG}
-                <Spacer y={1} />
-                <FaAnglesDown className="m-auto" />
-                <Spacer y={1} />
+                {props.isSafari &&
+                    <>
+                        <Spacer y={8} />
+                        <div><FaAnglesDown className="m-auto text-[#FF4429]" /></div>
+                    </>
+                }
+
+                {props.isChrome &&
+                    <>
+                        <div><FaAnglesUp className="fixed top-1 right-1 text-[#FF4429]" /></div>
+                    </>
+                }
+
+                {props.isFireFox &&
+                    <>
+                        <div><FaAnglesDown className="fixed bottom-2 right-5 text-[#FF4429]" /></div>
+                    </>
+                }
             </div>
-        </div>
+        </div >
     )
 
 }
