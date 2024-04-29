8fa2a0fc1e3f152a89e3ec18a7e34ab9c46b8214
Kuan Huang <huangkuan@gmail.com>
2024-04-29 14:11:20 -0400

    removed an empty line

--- src/app/img2vid/page.tsx
+++ src/app/img2vid/page.tsx
@@ -192,7 +192,6 @@ export default function Page() {
                             isLoading={isGeneratingVideo}
                             onPress={handleGenerateVideoClick}>Render
                         </PrimaryButton>
-
                     }
 
                     {isButtonNew &&
