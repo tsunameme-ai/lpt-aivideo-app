b20e37893755318036af8f075abe87d252ed78d0
CJ Jiang <cxjiang@gmail.com>
2024-04-29 16:17:50 -0400

    redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,38 @@
 'use client'
 import { useRouter } from 'next/navigation'
-import { useGenerationContext } from '@/context/generation-context'
 import { useEffect, useState } from 'react'
 import { usePrivy } from '@privy-io/react-auth'
-import { fetchAssetsByUser } from '@/actions/stable-diffusion'
 
 export default function Home() {
-  const { authenticated, user, ready } = usePrivy()
-  const router = useRouter()
-  const gContext = useGenerationContext()
-  const [hasRedirected, setRedirected] = useState<boolean>(false)
-  useEffect(() => {
-    if (gContext.i2vOutputs.length > 0) {
-      gotoPage('/gallery')
+    const { authenticated, user, ready } = usePrivy()
+    const router = useRouter()
+    const [hasRedirected, setRedirected] = useState<boolean>(false)
+    const redirect = async () => {
+        if (hasRedirected) {
+            return
+        }
+        if (ready) {
+            if (authenticated && user) {
+                gotoPage('/gallery')
+            }
+            else {
+                gotoPage('/welcome')
+            }
+        }
     }
-  }, [])
-  const redirect = async () => {
-    if (hasRedirected) {
-      return
+    const gotoPage = (path: string) => {
+        setRedirected(true)
+        router.replace(path)
     }
-    if (ready) {
-      if (authenticated && user) {
-        const data = await fetchAssetsByUser(user.id, 1)
-        gotoPage(data.items?.length > 0 ? '/gallery' : '/welcome')
-      }
-      else {
-        gotoPage('/welcome')
-      }
-    }
-  }
-  const gotoPage = (path: string) => {
-    setRedirected(true)
-    router.replace(path)
-  }
-  useEffect(() => {
-    redirect()
-  }, [ready])
+    useEffect(() => {
+        redirect()
+    }, [ready])
 
-  return (
-    <>
-      <div className='w-full h-svh flex items-center justify-center'>
-        <div className='font-bold text-primary text-5xl'>TSUNAMEME</div>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className='w-full h-svh flex items-center justify-center'>
+                <div className='font-bold text-primary text-5xl'>TSUNAMEME</div>
+            </div>
+        </>
+    )
 }
