5dc05ecbbba337a55ee9f40479529034061056ff
CJ Jiang <cxjiang@gmail.com>
2024-05-11 12:30:36 -0400

    Fix image

--- src/components/auth-indicator/inline.tsx
+++ src/components/auth-indicator/inline.tsx
@@ -52,7 +52,7 @@ const AuthInline: React.FC<AuthPromoProps> = (props: AuthPromoProps) => {
             {(!authenticated || !user) &&
                 <>
                     <Spacer y={4} />
-                    <SecondaryButton onPress={logInIfNot}>{authenticated && user ? 'save' : 'Login to save'}</SecondaryButton>
+                    <SecondaryButton onPress={logInIfNot}>{authenticated && user ? 'Save' : 'Login to save'}</SecondaryButton>
                 </>
             }
         </>
